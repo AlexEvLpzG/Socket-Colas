e9c2ea06e68cb3ce68ac68ab9807bb2ab22ac186
AlexiSkyline <ilegal_sprite@outlook.com>
2022-02-20 00:03:51 -0600

    Mostrando los cambios de los Tickets pendientes por atender en tiempo real

--- Controllers/Controller.js
+++ Controllers/Controller.js
@@ -3,14 +3,17 @@ const TicketControl = require( '../Models/Ticket-Control' );
 const ticketControl = new TicketControl();
 
 const socketController = ( socket ) => {
+    // * Cuando un cliente se conecta
     socket.emit( 'ultimo-ticket', ticketControl.ultimo );
     socket.emit( 'estado-actual', ticketControl.ultimos4 );
-    
+    socket.emit( 'tickets-pendientes', ticketControl.tikets.length );
+
     socket.on( 'siguiente-ticket', ( payload, callback ) => {
         const siguiente = ticketControl.siguiente();
         callback( siguiente );
 
         // TODO: Notificar que hay un nuevo ticket pendiente de asignar
+        socket.broadcast.emit( 'tickets-pendientes', ticketControl.tikets.length );
     });
 
     socket.on( 'atender-ticket', ({ escritorio }, callback ) => {
@@ -22,6 +25,8 @@ const socketController = ( socket ) => {
 
         // Todo: Notificar cambio en los ultimos4
         socket.broadcast.emit( 'estado-actual', ticketControl.ultimos4 );
+        socket.emit( 'tickets-pendientes', ticketControl.tikets.length );
+        socket.broadcast.emit( 'tickets-pendientes', ticketControl.tikets.length );
 
         if( !ticket ) {
             callback({ ok: false, msg: 'Ya no hay tockets pendientes' });
